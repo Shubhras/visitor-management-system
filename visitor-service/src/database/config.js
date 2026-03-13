0c8420c29a8f0dd7f140d675ed8f1d0818ef55c9
Kapil Singh <kapilkaashi@gmail.com>
2026-03-13 14:33:34 +0530

    Add Sequelize Config Files and Environment Files

---
+++ visitor-service/src/database/config.js
@@ -0,0 +1,30 @@
+// Sequelize CLI uses this file to know which database to connect to
+// when running migrations from the command line.
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME || 'visitor_db',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: parseInt(process.env.DB_PORT) || 3306,
+    dialect: 'mysql',
+  },
+  staging: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT) || 3306,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT) || 3306,
+    dialect: 'mysql',
+  },
+};
